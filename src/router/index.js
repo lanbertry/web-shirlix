f01611ca2e1b83d2708d967ada4f57490e840b04
Rose Baqueran <lanberttecson@gmail.com>
2025-08-02 19:29:44 +0800

    feat: added Login Form

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginVue from '@/views/LoginVue.vue'
+import LoginView from '@/views/auth/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: LoginVue,
+      component: LoginView,
     },
   ],
 })
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar class="px-3" color="grey-lighten-3">
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text="Toggle Theme"
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="6" class="mx-auto">
+              <v-card class="mx-auto" prepend-icon="mdi-account" subtitle="The #1 Vue UI Library">
+                <template v-slot:title>
+                  <span class="font-weight-black">Welcome to Vuetify</span>
+                </template>
+
+                <v-card-text class="bg-surface-light pt-4">
+                  <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Email" type="email" variant="outlined"></v-text-field>
+
+                    <v-text-field
+                      label="Password"
+                      type="password"
+                      variant="outlined"
+                    ></v-text-field>
+
+                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+
+      <v-footer color="grey-lighten-3" border app> 2025 - Sherlix Meatshop</v-footer>
+    </v-app>
+  </v-responsive>
+</template>
