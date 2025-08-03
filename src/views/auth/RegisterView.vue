d5ab9d4f37ba666594a3c8855bfb30af359795da
Rose Baqueran <lanberttecson@gmail.com>
2025-08-03 14:41:15 +0800

    feat: updated applayout and views

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,77 +1,65 @@
 <script setup>
-import { ref } from 'vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+import { useDisplay } from 'vuetify'
 
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+const { mobile } = useDisplay()
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3" color="grey-lighten-3">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-16">
+          <v-card class="mx-auto" elevation="24">
+            <v-card-title class="text-center">
+              <v-img
+                class="mx-auto"
+                src="/images/logo-favicon.png"
+                :width="mobile ? '75%' : '25%'"
+                rounded="circle"
+              ></v-img>
+              <h3 class="font-weight-black text-center">Shirlix Meatshop</h3>
+              <p>Register Form</p>
+            </v-card-title>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-row>
+                  <v-col cols="6">
+                    <v-text-field label="First Name" variant="outlined"></v-text-field>
+                  </v-col>
+                  <v-col><v-text-field label="Last Name" variant="outlined"></v-text-field></v-col>
+                </v-row>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto pt-16">
-              <v-card class="mx-auto" prepend-icon="mdi-login" subtitle="Register" elevation="24">
-                <template v-slot:title>
-                  <span class="font-weight-black">Shirlex Meatshop</span>
-                </template>
+                <v-text-field label="Email" variant="outlined"></v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-row>
-                      <v-col cols="6">
-                        <v-text-field label="First Name" variant="outlined"></v-text-field>
-                      </v-col>
-                      <v-col
-                        ><v-text-field label="Last Name" variant="outlined"></v-text-field
-                      ></v-col>
-                    </v-row>
+                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
 
-                    <v-text-field label="Email" variant="outlined"></v-text-field>
+                <v-text-field
+                  label="Password Confirmation"
+                  type="password"
+                  variant="outlined"
+                ></v-text-field>
 
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
+                <v-btn
+                  class="mt-2"
+                  color="primary"
+                  type="submit"
+                  block
+                  prepend-icon="mdi-account-plus"
+                  >Register</v-btn
+                >
+              </v-form>
 
-                    <v-text-field
-                      label="Password Confirmation"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
-
-                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-                  </v-form>
-
-                  <v-divider class="my-5"></v-divider>
-                  <h5 class="text-center">
-                    Already have an account?
-                    <RouterLink class="text-primary" to="/">Click here to Login</RouterLink>
-                  </h5>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer color="grey-lighten-3" border app> 2025 - Sherlix Meatshop</v-footer>
-    </v-app>
-  </v-responsive>
+              <v-divider class="my-5"></v-divider>
+              <h5 class="text-center">
+                Already have an account?
+                <RouterLink class="text-primary" to="/">Click here to Login</RouterLink>
+              </h5>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row></template
+    >
+  </AppLayout>
 </template>
