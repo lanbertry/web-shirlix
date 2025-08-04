d0079e47db5e02ccbb0be06196f6c83397fab681
Rose Baqueran <lanberttecson@gmail.com>
2025-08-04 14:16:05 +0800

    feat: modified text field added icons

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,8 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const visible = ref(false)
+</script>
+
 <template>
   <v-form fast-fail @submit.prevent>
-    <v-text-field label="Email" type="email" variant="outlined"></v-text-field>
+    <v-text-field prepend-inner-icon="mdi-email-outline" label="Email" type="email"></v-text-field>
 
-    <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+    <v-text-field
+      hint="Enter your password to access this website"
+      label="Password"
+      :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+      :type="visible ? 'text' : 'password'"
+      prepend-inner-icon="mdi-lock-outline"
+      @click:append-inner="visible = !visible"
+    ></v-text-field>
 
     <v-btn class="mt-2" color="primary" type="submit" block prepend-icon="mdi-login">Login</v-btn>
   </v-form>
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,17 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+
+const visible = ref(false)
+</script>
+
 <template>
   <v-form fast-fail @submit.prevent>
     <v-row>
       <v-col cols="6">
-        <v-text-field label="First Name" variant="outlined"></v-text-field>
+        <v-text-field color="primary" label="First Name"></v-text-field>
       </v-col>
-      <v-col><v-text-field label="Last Name" variant="outlined"></v-text-field></v-col>
+      <v-col><v-text-field color="primary" label="Last Name"></v-text-field></v-col>
     </v-row>
 
-    <v-text-field label="Email" variant="outlined"></v-text-field>
+    <v-text-field
+      prepend-inner-icon="mdi-email-outline"
+      color="primary"
+      label="Email"
+    ></v-text-field>
 
-    <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+    <v-row>
+      <v-col>
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          prepend-inner-icon="mdi-lock-outline"
+          @click:append-inner="visible = !visible"
+          color="primary"
+          label="Password"
+        ></v-text-field>
+      </v-col>
 
-    <v-text-field label="Password Confirmation" type="password" variant="outlined"></v-text-field>
+      <v-col>
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          color="primary"
+          label="Password Confirmation"
+        ></v-text-field
+      ></v-col>
+    </v-row>
 
     <v-btn class="mt-2" color="primary" type="submit" block prepend-icon="mdi-account-plus"
       >Register</v-btn
